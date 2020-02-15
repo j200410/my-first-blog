7df59e37b81bdc82d2ee543fdca2736c0ae532b2
j200410 <marishka200410@gmail.com>
2020-02-15 17:10:33 +0200

    modified urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
-
+from blog.models import Post
 from django.shortcuts import render, get_object_or_404
 from django.urls import path
 from . import views
@@ -12,6 +12,6 @@ urlpatterns = [
 ]
 
 def post_detail(request, pk):
-    #Post.objects.get(pk=pk)
+    Post.objects.get(pk=pk)
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
